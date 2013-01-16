75f112a7d2f0ac0490c968cfbb890322881dfd44
Matt Mondok <matt.mondok@gmail.com>
2013-01-15 23:42:19 -0500

    Added global notification callback so applications can be notified after an event has occurred.

---
+++ Notifly/ZConstants.h
@@ -0,0 +1,13 @@
+//
+//  ZConstants.h
+//  Notifly
+//
+//  Created by Matthew Mondok on 1/15/13.
+//  Copyright (c) 2013 rob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZConstants : NSObject
+  FOUNDATION_EXPORT NSString *const kNotiflyNotificationName;
+@end
